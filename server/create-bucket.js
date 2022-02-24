3ba5e4753a58a55ecf409b91dba82934f7942d4d
Noemiegrau <noemiegrau@outlook.fr>
2022-02-24 15:30:53 -0800

    created DynamoDB instance + created API endpoints

--- server/create-bucket.js
+++ server/create-bucket.js
@@ -4,7 +4,7 @@ const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
 // Set the region
-AWS.config.update({region: 'us-east-2'});
+AWS.config.update({region: 'us-west-1'});
 
 // Create S3 service object
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
